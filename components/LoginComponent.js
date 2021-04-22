75e8536a34a8f49e113716b0f1bdaa3b66f5ef6a
Tatum Bray <tatumrbray@gmail.com>
2021-04-22 12:13:45 -0600

    Login Sign Up styling pt 2

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
-import { Text, View, StyleSheet,  } from "react-native"
+import SignUp from "./SignUpComponent"
+import { Text, View, StyleSheet, TextInput } from "react-native"
 import { Card, Input, Button } from "react-native-elements"
 import { ScrollView } from "react-native-gesture-handler"
 
@@ -11,7 +12,7 @@ class Login extends Component {
       <View style={styles.viewBackground}>
         <Text style={styles.loginHeader}>Login</Text>
         <Text style={styles.inputHeader}>Email</Text>
-        <Input
+        <TextInput
           style={styles.input}
           placeholder='example@example.com'
           placeholderTextColor={"#D7EB5A"}
@@ -20,20 +21,39 @@ class Login extends Component {
           <Text>Password</Text>
           <Text style={styles.forgotPassword}>Forgot password?</Text>
         </Text>
-        <Input
+        <TextInput
           style={styles.input}
           placeholder='password'
           placeholderTextColor={"#D7EB5A"}
         />
-        <Button title={"Sign In"} titleStyle={{color: "#232323"}} buttonStyle={{ backgroundColor: "#D7EB5A"}}/>
-        <Text style={styles.shiftLeft}>
-          <Text style={styles.forgotPassword}>Don't have an account?</Text>
-          <Text style={styles.signUp}>Sign Up</Text>
+        <Button
+          title={"Login"}
+          titleStyle={{ color: "#232323", fontSize: 24 }}
+          buttonStyle={{
+            backgroundColor: "#D7EB5A",
+            marginTop: 38,
+            marginLeft: 25,
+            marginRight: 25,
+            padding: 18,
+            borderRadius: 17,
+          }}
+        />
+        <Text style={styles.forgotPassword}>
+          <Text>Don't have an account?</Text>
+          {/* <SignUpLink /> */}
         </Text>
       </View>
     )
   }
 }
+
+// function SignUpLink({ navigation }) {
+//   return (
+//     <View>
+//       <Button title={"Sign Up"} onPress={() => navigation.navigate("SignUp")} />
+//     </View>
+//   )
+// }
 
 const styles = StyleSheet.create({
   viewBackground: {
@@ -46,6 +66,7 @@ const styles = StyleSheet.create({
     color: "#D7EB5A",
     marginLeft: 30,
     marginBottom: 20,
+    marginTop: 40,
   },
   inputHeader: {
     fontFamily: "asap-regular",
@@ -57,27 +78,27 @@ const styles = StyleSheet.create({
   input: {
     marginLeft: 20,
     marginRight: 20,
+    marginBottom: 30,
     backgroundColor: "#000000",
-    borderBottomColor: '#232323',
+    borderBottomColor: "#232323",
     borderRadius: 5,
-    padding: 8,
+    padding: 12,
   },
   forgotPassword: {
     fontFamily: "asap-regular",
     fontSize: 15,
     color: "#78797B",
-    alignSelf: "flex-end",
+    marginTop: 20,
     marginBottom: 20,
+    marginLeft: 20,
   },
   signUp: {
     flex: 1,
     fontFamily: "asap-regular",
     fontSize: 15,
     color: "#8B51F5",
-    flexDirection: 'row-reverse',
+    flexDirection: "row-reverse",
     marginBottom: 20,
-  },
-  shiftLeft: {
     marginLeft: 20,
   },
 })
--- components/SignUpComponent.js
+++ components/SignUpComponent.js
@@ -1,16 +1,108 @@
 import React, { Component } from "react"
-import { Text, View, StyleSheet } from "react-native"
-import { Card } from "react-native-elements"
+import { Text, View, StyleSheet, TextInput } from "react-native"
+import { Card, Input, Button } from "react-native-elements"
 import { ScrollView } from "react-native-gesture-handler"
 
 class SignUp extends Component {
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: "#232323" }}>
-        <Text></Text>
-      </View>
+      <ScrollView>
+        <View style={styles.viewBackground}>
+          <Text style={styles.loginHeader}>Sign Up</Text>
+          <Text style={styles.inputHeader}>Name</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='John Doe'
+            placeholderTextColor={"#D7EB5A"}
+          />
+          <Text style={styles.inputHeader}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='example@example.com'
+            placeholderTextColor={"#D7EB5A"}
+          />
+          <Text style={styles.inputHeader}>Username</Text>
+          <TextInput
+            style={styles.input}
+            placeholder='example123'
+            placeholderTextColor={"#D7EB5A"}
+          />
+          <Text style={styles.inputHeader}>Password</Text>
+          <TextInput
+            style={styles.input}
+            selectionColor={"red"}
+            placeholder='password'
+            placeholderTextColor={"#D7EB5A"}
+          />
+          <Button
+            title={"Sign Up"}
+            titleStyle={{ color: "#232323", fontSize: 24 }}
+            buttonStyle={{
+              backgroundColor: "#D7EB5A",
+              marginTop: 38,
+              marginLeft: 25,
+              marginRight: 25,
+              padding: 18,
+              borderRadius: 17,
+            }}
+          />
+          <Text style={styles.shiftLeft}>
+            <Text style={styles.forgotPassword}>Already have an account?</Text>
+            <Text style={styles.signUp}>Login</Text>
+          </Text>
+        </View>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  viewBackground: {
+    flex: 1,
+    backgroundColor: "#232323",
+  },
+  loginHeader: {
+    fontFamily: "KoHo-bold",
+    fontSize: 35,
+    color: "#D7EB5A",
+    marginLeft: 30,
+    marginBottom: 20,
+    marginTop: 40,
+  },
+  inputHeader: {
+    fontFamily: "asap-regular",
+    fontSize: 20,
+    color: "#D7EB5A",
+    marginLeft: 30,
+    marginBottom: 20,
+  },
+  input: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 30,
+    backgroundColor: "#090909",
+    borderBottomColor: "#232323",
+    borderRadius: 5,
+    padding: 12,
+  },
+  forgotPassword: {
+    fontFamily: "asap-regular",
+    fontSize: 15,
+    color: "#78797B",
+    alignSelf: "flex-end",
+    marginBottom: 20,
+  },
+  signUp: {
+    flex: 1,
+    fontFamily: "asap-regular",
+    fontSize: 15,
+    color: "#8B51F5",
+    flexDirection: "row-reverse",
+    marginBottom: 20,
+  },
+  shiftLeft: {
+    marginLeft: 20,
+  },
+})
 
 export default SignUp

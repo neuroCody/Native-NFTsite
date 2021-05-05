f1a2eb91695c8e01a4e0a6b8529f25ab1ed4b1fa
Tatum Bray <tatumrbray@gmail.com>
2021-05-05 11:43:22 -0600

    Redux store, reducers, actions set up

--- App.js
+++ App.js
@@ -1,41 +1,38 @@
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import Main from './components/MainComponent';
-import { AppLoading } from 'expo';
+import React, { useState } from "react"
+import { Provider } from "react-redux"
+import { state } from "./redux/store"
+import * as Font from "expo-font"
+import Main from "./components/MainComponent"
+import { AppLoading } from "expo"
 
 //Imported fonts
 const getFonts = () => {
   return Font.loadAsync({
     // <p> elemnts
-    'asap-regular': require('./assets/fonts/Asap-Regular.ttf'),
+    "asap-regular": require("./assets/fonts/Asap-Regular.ttf"),
 
     // site headers
-    'KoHo-regular': require('./assets/fonts/KoHo-Regular.ttf'),
-    'KoHo-bold': require('./assets/fonts/KoHo-Bold.ttf'),
+    "KoHo-regular": require("./assets/fonts/KoHo-Regular.ttf"),
+    "KoHo-bold": require("./assets/fonts/KoHo-Bold.ttf"),
 
     // logo font
-    'satisfy-regular': require('./assets/fonts/Satisfy-Regular.ttf'),
+    "satisfy-regular": require("./assets/fonts/Satisfy-Regular.ttf"),
   })
 }
 
-
-
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [fontsLoaded, setFontsLoaded] = useState(false)
 
-  if(fontsLoaded){
+  if (fontsLoaded) {
     return (
+      // <Provider store={state}>
       <Main />
-    );
-  }
-  else{
-    return(
-      <AppLoading 
-        startAsync={getFonts}
-        onFinish={() => setFontsLoaded(true)}
-      />
+      // </Provider>
+    )
+  } else {
+    ///Unsure whether to wrap APPLOADING in PROVIDER
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
     )
   }
-  
-  
 }
---
+++ redux/reducers.js
@@ -0,0 +1,11 @@
+// import { IMAGES } from '../arrays/images'
+
+// const initialState = {
+//   showModal: false,
+//   id: IMAGES,
+//   title: IMAGES,
+//   image: IMAGES,
+//   price: IMAGES,
+//   views: IMAGES,
+//   date: IMAGES,
+// }

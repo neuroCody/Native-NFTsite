ca06b44f1909b77cdc54d7617096d3de190d2ff6
Cody Sullivan <76827473+neuroCody@users.noreply.github.com>
2021-07-21 11:42:44 -0600

    User Profile Navigation & Styling

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useLayoutEffect } from "react"
 import {
   StyleSheet,
   Text,
@@ -6,74 +6,154 @@ import {
   ScrollView,
   TouchableOpacity,
   Image,
+  SafeAreaView,
+  TextInput,
 } from "react-native"
 import FavoritesCarousel from "./FavoritesCarousel"
 import { Icon } from "react-native-elements"
-
+import { createStackNavigator } from '@react-navigation/stack';
+import * as Animatable from "react-native-animatable"
 import { connect } from "react-redux"
 
 function mapStateToProps(state) {
-  console.log(state)
+  //console.log(state)
   return { image: state.image }
 }
 
-export default function UserProfile(props) {
-  console.log(`this is LLLLL: ${props}`)
+
+
+function UserProfileScreen(props) {
+  const [shouldShow, setShouldShow] = useState(false)
+
+  useLayoutEffect(() => {
+    props.navigation.setOptions({
+      headerRight: () => (
+        <Icon
+          name='search'
+          type='font-awesome'
+          iconStyle={{
+            color: "#D7EB5A",
+            margin: 20,
+            marginBottom: 35,
+          }}
+          size={32}
+          onPress={() => setShouldShow(!shouldShow)}
+        />
+      ),
+    })
+  })
+
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.headerView}>
-        <View style={styles.coverImage}>
-            <Image
-              source={require("./images/galaxyraccoon.jpg")}
-              style={styles.coverImage}
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#232323" }}>
+      {shouldShow ? (
+        <Animatable.View
+          animation='slideInDown'
+          duration={900}
+          style={{
+            height: 40,
+            backgroundColor: "#232323",
+            justifyContent: "center",
+          }}
+        >
+          <TextInput
+            placeholder='Search'
+            style={styles.input}
+            // onChangeText={onChangeText}
+            // value={text}
+          />
+        </Animatable.View>
+      ) : null}
+      <ScrollView style={styles.container}>
+        <View style={styles.headerView}>
+          <View style={styles.coverImage}>
+              <Image
+                source={require("./images/galaxyraccoon.jpg")}
+                style={styles.coverImage}
+              />
+            <View style={styles.editBox}>
+              <Text style={styles.editText}>Edit Profile</Text>
+            </View>
+          </View>
+          
+          <View style={styles.profileImage}>
+            <Image style={styles.profileImage} source={require("./images/elon_and_doge.jpg") } />
+          </View>
+          <View style={styles.location}>
+            <Icon
+              name='map-marker'
+              type='font-awesome'
+              iconStyle={{
+                color: "#F93737",
+                marginRight: 6,
+              }}
+              size={15}
             />
-          <View style={styles.editBox}>
-            <Text style={styles.editText}>Edit Profile</Text>
+            <Text style={{ color: "#EBEBEB" }}>Location</Text>
+          </View>
+
+          <Text style={styles.username}>Elon Doge</Text>
+          <View style={styles.bioView}>
+            <Text style={styles.bioText}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur,
+              corporis fugit
+            </Text>
           </View>
         </View>
-        
-        <View style={styles.profileImage}>
-          <Image style={styles.profileImage} source={require("./images/elon_and_doge.jpg") } />
+        {/**FAVORITES */}
+        <View style={styles.favoritesView}>
+          <Text style={styles.favoritesHeader}>Favorites</Text>
+          {/* <View style={styles.favoritesBox}> */}
+          <FavoritesCarousel/>
+          {/* </View> */}
         </View>
-        <View style={styles.location}>
-          <Icon
-            name='map-marker'
-            type='font-awesome'
-            iconStyle={{
-              color: "#F93737",
-              marginRight: 6,
-            }}
-            size={15}
-          />
-          <Text style={{ color: "#EBEBEB" }}>Location</Text>
-        </View>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.btnText}>Account Settings</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.btnText}>Orders</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttons}>
+          <Text style={styles.btnText}>Messages</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
 
-        <Text style={styles.username}>Elon Doge</Text>
-        <View style={styles.bioView}>
-          <Text style={styles.bioText}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur,
-            corporis fugit
-          </Text>
-        </View>
-      </View>
-      {/**FAVORITES */}
-      <View style={styles.favoritesView}>
-        <Text style={styles.favoritesHeader}>Favorites</Text>
-        {/* <View style={styles.favoritesBox}> */}
-        <FavoritesCarousel/>
-        {/* </View> */}
-      </View>
-      {/* <Flatlist>flatlist data link to stack navigator</Flatlist> */}
-      <TouchableOpacity style={styles.buttons}>
-        <Text style={styles.btnText}>Account Settings</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.buttons}>
-        <Text style={styles.btnText}>Orders</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.buttons}>
-        <Text style={styles.btnText}>Messages</Text>
-      </TouchableOpacity>
-    </ScrollView>
+const Stack = createStackNavigator()
+
+export default function UserProfile({ navigation }) {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Visualux'
+        component={UserProfileScreen}
+        options={{
+          Headertitle: "Visualux",
+          headerStyle: {
+            backgroundColor: "#232323",
+          },
+          headerTitleStyle: {
+            fontFamily: "satisfy-regular",
+            color: "#F2F2F2",
+            fontSize: 30,
+            marginLeft: "33%",
+          },
+          headerLeft: () => (
+            <Icon
+                name='bars'
+                type='font-awesome'
+                iconStyle={{
+                  color: "#8B51F5",
+                  margin: 15,
+                }}
+                size={35}
+                onPress={() => navigation.toggleDrawer()}
+              />
+          ),
+        }}
+      />
+    </Stack.Navigator>
   )
 }
 
@@ -174,7 +254,18 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "#EBEBEB",
     marginLeft: 10,
-  }
+  },
+  input: {
+    height: 34,
+    borderColor: "#D7EB5A",
+    borderWidth: 1,
+    borderRadius: 5,
+    color: "#D7EB5A",
+    backgroundColor: "#232323",
+    paddingLeft: 10,
+    margin: 3,
+    fontFamily: "KoHo-regular",
+  },
 })
 
-connect(mapStateToProps)(UserProfile)
+connect(mapStateToProps)(UserProfileScreen)
